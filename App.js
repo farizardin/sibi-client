ef39e26f712c4fe458cff30d403644d60880e3d3
Fariz Ardin Adhiyaksa <fariz.ardin@gmail.com>
2023-06-07 23:16:10 +0700

    SIBI Client Project

---
+++ App.js
@@ -0,0 +1,342 @@
+import { StyleSheet, Text, View, Button, SafeAreaView, TouchableOpacity, Dimensions, Modal, TextInput } from 'react-native';
+import { useEffect, useState, useRef } from 'react';
+import { Camera, CameraType } from 'expo-camera';
+import { Video } from 'expo-av';
+import { shareAsync } from 'expo-sharing';
+import { AntDesign } from '@expo/vector-icons';
+import * as MediaLibrary from 'expo-media-library';
+import axios from 'axios';
+import LoadingOverlay from './LoadingOverlay';
+// import { Modal } from 'react-native-modal';
+import { Icon } from 'react-native-elements';
+import { FloatingAction } from 'react-native-floating-action';
+
+
+
+// const SERVER_URL = 'http://192.168.0.37:1234/recognize/api';
+
+export default function App() {
+  let cameraRef = useRef();
+  const [type, setType] = useState(CameraType.back);
+  const [hasCameraPermission, setHasCameraPermission] = useState();
+  const [hasMicrophonePermission, setHasMicrophonePermission] = useState();
+  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
+  const [isRecording, setIsRecording] = useState(false);
+  const [video, setVideo] = useState();
+
+  const [imagePadding, setImagePadding] = useState(0);
+  const [ratio, setRatio] = useState('4:3');  // default is 4:3
+  const { height, width } = Dimensions.get('window');
+  const screenRatio = height / width;
+  const [isRatioSet, setIsRatioSet] =  useState(false)
+  const [isActive, setIsActive] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalSettingVisible, setSettingModalVisible] = useState(false);
+  const [predicted, setPredicted] = useState();  // default is 4:3
+  const [SERVER_URL, setInputValue] = useState('http://192.168.0.37:1234/recognize/api');
+  const [isLoading, setIsLoading] = useState(false);
+  const actions = [
+    { text: 'Capture', icon: <Icon name="camera" size={20} color="white" />, name: 'capture' },
+    // Add more buttons as needed
+  ];
+  useEffect(() => {
+    (async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+
+      setHasCameraPermission(cameraPermission.status === "granted");
+      setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
+    })();
+  }, []);
+
+  useEffect(() => {
+    let timer = null;
+    if(isActive){
+      timer = setInterval(() => {
+        console.log(seconds);
+        setSeconds((seconds) => seconds + 1);
+      }, 1000);
+    }
+    return () => {
+      clearInterval(timer);
+    };
+  });
+
+  const prepareRatio = async () => {
+    let desiredRatio = '16:9';  // Start with the system default
+    // This issue only affects Android
+    if (Platform.OS === 'android') {
+      const ratios = await cameraRef.current.getSupportedRatiosAsync();
+
+      // Calculate the width/height of each of the supported camera ratios
+      // These width/height are measured in landscape mode
+      // find the ratio that is closest to the screen ratio without going over
+      let distances = {};
+      let realRatios = {};
+      let minDistance = null;
+      for (const ratio of ratios) {
+        const parts = ratio.split(':');
+        const realRatio = parseInt(parts[0]) / parseInt(parts[1]);
+        realRatios[ratio] = realRatio;
+        // ratio can't be taller than screen, so we don't want an abs()
+        const distance = screenRatio - realRatio; 
+        distances[ratio] = realRatio;
+        if (minDistance == null) {
+          minDistance = ratio;
+        } else {
+          if (distance >= 0 && distance < distances[minDistance]) {
+            minDistance = ratio;
+          }
+        }
+      }
+      // set the best match
+      desiredRatio = minDistance;
+      //  calculate the difference between the camera width and the screen height
+      const remainder = Math.floor(
+        (height - realRatios[desiredRatio] * width) / 2
+      );
+      // set the preview padding and preview ratio
+      setImagePadding(remainder / 2);
+      setRatio(desiredRatio);
+      // Set a flag so we don't do this 
+      // calculation each time the screen refreshes
+      setIsRatioSet(true);
+      console.log(screenRatio)
+    }
+  };
+
+  // the camera must be loaded in order to access the supported ratios
+  const setCameraReady = async() => {
+    if (!isRatioSet) {
+      await prepareRatio();
+    }
+  };
+
+  if (hasCameraPermission === undefined) {
+    return <Text>Requestion permissions...</Text>
+  } else if (!hasCameraPermission) {
+    return <Text>Permission for camera not granted.</Text>
+  }
+
+  let recordVideo = () => {
+    setIsActive(true);
+    setIsRecording(true);
+    let options = {
+      quality: "1080p",
+      maxDuration: 60,
+      mute: false
+    };
+
+    // console.log(imagePadding)
+
+    cameraRef.current.recordAsync(options).then((recordedVideo) => {
+      setVideo(recordedVideo);
+      setIsRecording(false);
+    });
+  };
+
+  let stopRecording = () => {
+    setSeconds(0);
+    setIsActive(false);
+    setIsRecording(false);
+    cameraRef.current.stopRecording();
+  };
+
+  const handleInputChange = (text) => {
+    setInputValue(text);
+  };
+
+  const uploadVideo = async () => {
+    if (video) {
+      const formData = new FormData();
+      formData.append('video', {
+        uri: video.uri,
+        type: 'video/mp4',
+        name: 'video.mp4',
+      });
+      console.log(SERVER_URL, formData)
+      var headers = {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+    }
+    setIsLoading(true);
+    var response = await axios.post(SERVER_URL, formData, headers);
+    console.log(response.data.data.predicted_word);
+    setIsLoading(false);
+    setModalVisible(true);
+    setPredicted(response.data.data.predicted_word)
+      // predicted = response.data.predicted_word;
+    }
+  };
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+
+  if (video) {
+    let shareVideo = () => {
+      shareAsync(video.uri).then(() => {
+        setVideo(undefined);
+      });
+    };
+
+    let saveVideo = () => {
+      MediaLibrary.saveToLibraryAsync(video.uri).then(() => {
+        setVideo(undefined);
+      });
+    };
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.video}>
+          <Video
+            style={styles.video}
+            source={{uri: video.uri}}
+            useNativeControls
+            resizeMode='contain'
+            isLooping
+          />
+          <Button title="Recognize" onPress={uploadVideo} />
+          {hasMediaLibraryPermission ? <Button title="Save" onPress={saveVideo} /> : undefined}
+          <Button color="darkred" title="Discard" onPress={() => setVideo(undefined)} />
+        </View>
+        <LoadingOverlay isVisible={isLoading} />
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => setModalVisible(false)}
+        >
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <Text style={styles.modalText}>Prediksi kata atau kalimat</Text>
+              <Text>{predicted}</Text>
+              <Button title="Close" onPress={() => setModalVisible(false)} />
+            </View>
+          </View>
+        </Modal>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity  style={styles.floatingButton} onPress={() => setSettingModalVisible(true)}>
+        <AntDesign name="setting" size={24} color="#fff" />
+      </TouchableOpacity>
+      {/* <FloatingAction
+        actions={actions}
+        color="#2F54EB"  // Set button color
+        overlayColor="rgba(0, 0, 0, 0.5)"  // Set button overlay color
+      /> */}
+      <Camera
+        onCameraReady={setCameraReady}
+        type={type}
+        ratio={ratio}
+        style={[styles.container, { marginBottom: imagePadding * 4}]}
+        ref={cameraRef}
+      />
+      <View style={[styles.controls, {marginBottom: imagePadding }]}>
+      <Text style={styles.buttonText}>
+        {seconds}
+      </Text>
+        <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={isRecording ? stopRecording : recordVideo}
+        >
+          <Text style={styles.buttonText}>
+            {isRecording ? "Stop Recording" : "Record Video"}
+          </Text>
+        </TouchableOpacity>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalSettingVisible}
+          onRequestClose={() => setModalVisible(false)}
+        >
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <Text style={styles.modalText}>Pengaturan</Text>
+              <TextInput
+                style={styles.inputField}
+                placeholder="Enter endpoint"
+                value={SERVER_URL}
+                onChangeText={handleInputChange}
+              />
+              <Button style={{marginTop: 'auto'}} title="Close" onPress={() => setSettingModalVisible(false)} />
+            </View>
+          </View>
+        </Modal>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  controls: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  button: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 5,
+    marginHorizontal: 10,
+  },
+  preview_button: {
+    padding: 20,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  video: {
+    flex: 1,
+  },
+  floatingButton: {
+    position: 'absolute',
+    bottom: 40,
+    right: 20,
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: '#007AFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 5,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 20,
+    width: '80%', // Adjust the width as desired
+    borderRadius: 8,
+  },
+  modalText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+});
